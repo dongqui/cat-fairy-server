be14ccbd1c43a928086a6c69ee4cb1feb426c2c6
dongqui <kimwinner90@gmail.com>
2020-02-05 20:53:37 +0900

    new structure

--- cat_fairy/apps/urls.py
+++ cat_fairy/apps/urls.py
@@ -1,4 +1,4 @@
-"""cat_fairy URL Configuration
+"""apps URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
