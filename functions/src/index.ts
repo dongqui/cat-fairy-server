15e928c6978e8e335f7667ffb3daf6a9707656c0
dongqui <dongqui9@gmail.com>
2020-08-03 23:36:45 +0900

    get years data from github page

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,26 @@
 import * as firebaseFunctions from 'firebase-functions';
 const functions = firebaseFunctions.region('asia-northeast1');
+import * as cheerio from "cheerio";
+import axios from 'axios';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+export const githubInfo = functions.https.onRequest(async (request, response) => {
+ if (request.method === 'GET') {
+  const { username } = request.query;
+  const data = (await axios.get(`https://github.com/${username}`, { responseType: 'text' })).data;
+  const $ = cheerio.load(data);
+  const years = $(".js-year-link")
+    .get()
+    .map((a: string) => {
+     const $a = $(a);
+     return {
+      href: $a.attr("href"),
+      text: $a.text().trim()
+     };
+    });
+  console.log(years);
+ }
+})
