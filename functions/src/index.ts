233ee1d049c59d59ac3d08955c01a77405ea859d
dongqui <dongqui9@gmail.com>
2020-08-07 01:39:02 +0900

    set data-date range

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,21 +9,30 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 export const githubInfo = functions.https.onRequest(async (request, response) => {
  if (request.method === 'GET') {
-   const startDate = 2020;
-  const { username } = request.query;
-  const githubFirstPage = (await axios.get(`https://github.com/${username}`, { responseType: 'text' })).data;
-  const $ = cheerio.load(githubFirstPage);
-  const targetYears = $(".js-year-link")
-    .get()
-    .map((a: string) => {
-     const $a = $(a);
-     return {
-      href: $a.attr("href"),
-      text: $a.text().trim()
-     };
-    })
-    .filter(a => Number(a.text) <= startDate);
+   const startDate = new Date('2020-05-06');
+   const { username } = request.query;
+   const githubFirstPage = (await axios.get(`https://github.com/${username}`, { responseType: 'text' })).data;
+   const $ = cheerio.load(githubFirstPage);
+   const targetYears = $(".js-year-link")
+     .get()
+     .map((a: string) => {
+       const $a = $(a);
+       return {
+         href: $a.attr("href"),
+         text: $a.text().trim()
+       };
+     })
+     .filter(a => Number(a.text) >= startDate.getFullYear());
 
-  
- }
+   for (const year of targetYears) {
+     const yearPage = (await axios.get(`https://github.com/${year.href}`, { responseType: 'text' })).data;
+     const $ = cheerio.load(yearPage);
+     const $days = $("rect.day");
+     const dateData = {
+       start: $($days.get(0)).attr("data-date"),
+       end: $($days.get($days.length - 1)).attr("data-date")
+     }
+     console.log(dateData)
+   }
+  }
 })
